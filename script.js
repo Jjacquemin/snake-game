ba4fce3be27d906e62f24180113ba5f88018c510
Joachim JACQUEMIN <joachim.jacquemin@cegedim-activ.com>
2020-04-26 12:08:13 +0200

    Mise en place d'un serpent qui va à droite

--- script.js
+++ script.js
@@ -1,27 +1,54 @@
 window.onload = function(){
+    var canvasWidth = 900;
+    var canvasHeight = 600;
+    var blockSize = 30;
     var canvas;
     var ctx;
     var delay = 100;
-    var xCoord = 0;
-    var yCoord = 0;
+    var snakee;
 
     function init(){
         canvas = document.createElement('canvas');
-        canvas.width = 900;
-        canvas.height = 600;
+        canvas.width = canvasWidth;
+        canvas.height = canvasHeight;
         canvas.style.border = "1px solid";
         document.body.appendChild(canvas);
         ctx = canvas.getContext('2d');
+        snakee = new Snake([[6,4],[5,4],[4,4]]);
         refreshCanvas();
     }
     
     function refreshCanvas(){
-        ctx.clearRect(0,0,canvas.width,canvas.height);
-        ctx.fillStyle = "#ff0000";
-        xCoord += 5;
-        yCoord += 5;
-        ctx.fillRect(xCoord, yCoord, 100, 50);
+        ctx.clearRect(0,0,canvasWidth,canvasHeight);
+        snakee.advance();
+        snakee.draw();
         setTimeout(refreshCanvas,delay);
+    }
+    
+    function drawBlock(ctx, position){
+        var x = position[0] * blockSize;
+        var y = position[1] * blockSize;
+        ctx.fillRect(x, y, blockSize, blockSize);
+    }
+
+    function Snake(body){
+        this.body = body;
+        
+        this.draw = function(){
+            ctx.save();
+            ctx.fillStyle = "#ff0000";
+            for(var i=0; i < this.body.length; i++){
+                drawBlock(ctx, this.body[i]);
+            }
+            ctx.restore();
+        };
+        
+        this.advance = function(){
+            var nextHeadPosition = this.body[0].slice(); // pour copier la tête et non faire une référence.
+            nextHeadPosition[0]++;
+            this.body.unshift(nextHeadPosition);
+            this.body.pop();
+        };
     }
     
     init();
