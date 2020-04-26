940ff3b553e1e1bb9f0839a6a3ebaa7071bd9c6f
Joachim JACQUEMIN <joachim.jacquemin@cegedim-activ.com>
2020-04-26 19:46:50 +0200

    Utilisation d'un paramètre rest sur le constructeur de serpent

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ window.onload = () => {
     const launch = () => {
         if (launchToDo){
             launchToDo = false;
-            snakee = new Snake([[6,4],[5,4],[4,4],[3,4],[2,4]], "right");
+            snakee = new Snake("right", [6,4],[5,4],[4,4],[3,4],[2,4]);
             applee = new Apple();
             score = 0;
             delay = 100;
@@ -103,7 +103,7 @@ window.onload = () => {
 
     class Snake {
         
-        constructor(body, direction){
+        constructor(direction, ...body){
             this.body = body;
             this.direction = direction;
             this.ateApple = false;
