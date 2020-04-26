66c8dd5f1eb193aa95247be260a2e71986833d7c
Joachim JACQUEMIN <joachim.jacquemin@cegedim-activ.com>
2020-04-26 10:02:53 +0200

    Déplacement du rectangle

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 window.onload = function(){
-    var canvas = document.createElement('canvas');
-    canvas.width = 900;
-    canvas.height = 600;
-    canvas.style.border = "1px solid";
-    document.body.appendChild(canvas);
-    var ctx = canvas.getContext('2d');
-    ctx.fillStyle = "#ff0000";
-    ctx.fillRect(30, 30, 100, 50);
+    var canvas;
+    var ctx;
+    var delay = 100;
+    var xCoord = 0;
+    var yCoord = 0;
+
+    function init(){
+        canvas = document.createElement('canvas');
+        canvas.width = 900;
+        canvas.height = 600;
+        canvas.style.border = "1px solid";
+        document.body.appendChild(canvas);
+        ctx = canvas.getContext('2d');
+        refreshCanvas();
+    }
+    
+    function refreshCanvas(){
+        ctx.clearRect(0,0,canvas.width,canvas.height);
+        ctx.fillStyle = "#ff0000";
+        xCoord += 5;
+        yCoord += 5;
+        ctx.fillRect(xCoord, yCoord, 100, 50);
+        setTimeout(refreshCanvas,delay);
+    }
+    
+    init();
+    
 }
