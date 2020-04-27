0d563ea57e6ff607839af3e78d948ff2f7c73730
Joachim JACQUEMIN <joachim.jacquemin@cegedim-activ.com>
2020-04-27 06:16:55 +0200

    Utilisation de includes

--- snake.js
+++ snake.js
@@ -46,7 +46,7 @@ export default class Snake {
             default:
                 throw("Invalid Direction");
         }
-        if (allowedDirections.indexOf(newDirection) > -1){
+        if (allowedDirections.includes(newDirection)){
             this.direction = newDirection;
         }
     }
