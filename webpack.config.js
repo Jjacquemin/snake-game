9415b6eafbe2f4a39673db85908110b506b82116
Joachim JACQUEMIN <joachim.jacquemin@cegedim-activ.com>
2020-04-27 08:19:07 +0200

    Implémentation webpack-dev-server via npm run start

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,9 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: "bundle.js"
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        open: true
     }
 }
